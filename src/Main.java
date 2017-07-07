74bb9aeb0450aca857d22f858e35e9e1d822fb07
James Whitehead <jameswhthead@gmail.com>
2017-07-07 15:16:52 +0100

    Working with JsonArray, should probably change to JsonObject to better match the Vehicle class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-/**
- * Created by James Whitehead on 07/07/2017.
- */
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src\\vehicles.json"));
+        JsonParser jsonParser = new JsonParser();
+        //JSON structure is <Search<Vehicle List>>
+        JsonArray jsonArray = jsonParser.parse(bufferedReader).getAsJsonObject().getAsJsonObject("Search").getAsJsonArray("VehicleList");
+        System.out.println(jsonArray.get(0));
+    }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+    private String sipp;
+    private String name;
+    private float price;
+    private String supplier;
+    private float rating;
+
+    public Vehicle() {
+
+    }
+}
